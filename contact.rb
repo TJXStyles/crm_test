c0e91b67f13b717bb52136aaea66a8def9e9b3f8
Bitmaker Labs <bitmakerlabs@cnc.local>
2013-06-11 15:23:11 -0400

    first working test of valid parameters

---
+++ contact.rb
@@ -0,0 +1,18 @@
+class Contact
+
+	# attr_accessor :id, :first_name, :last_name, :email, :notes, :twitter
+
+	def initialize(id, first_name, last_name, email, notes, twitter)
+		@id = id
+		@first_name = first_name
+		@last_name = last_name
+		@email = email
+		@notes = notes
+		@twitter = twitter
+	end	
+
+	# def add_contact
+		
+	# end
+
+end
---
+++ test_contact.rb
@@ -0,0 +1,20 @@
+require 'test/unit'
+require_relative 'contact'
+
+
+class TestContact < Test::Unit::TestCase
+
+	def test_initialize_the_params
+	#1. Write a test method for adding a contact that checked that the 6 attributes are present for a contact
+	# 	A contact should include information including these 6 attributes: ID, first name, lastname, email, notes and twitter
+	#2. Test fails - error should be csomething simple (i.e. there is not contact)
+	#3. Write production code
+	#4. run tests
+	# => a. If tests fail, rewrite production code
+	# => b. If successful, refactor.
+		contact = Contact.new(1, 'Jack', 'Cooc', 'gmail', 'notes', '@jackcooc')
+		assert contact.is_a?(Contact)
+	end
+
+	
+end
